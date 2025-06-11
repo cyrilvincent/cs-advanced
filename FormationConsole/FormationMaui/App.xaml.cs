514e335356ff9b44707bfeddfe0529e9a4ab5290
Cyril Vincent <contact@cyrilvincent.com>
2025-06-11 16:32:28 +0200

    Mycronic

---
+++ FormationConsole/FormationMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FormationMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FormationConsole/FormationMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FormationMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
